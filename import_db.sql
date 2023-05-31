adce02d18a5ee17fb246081fdd4558d809dad72f
Viktoria Czaran <vczaran@gmail.com>
2023-05-31 17:28:38 -0400

    Stuck on question_follows

--- import_db.sql
+++ import_db.sql
@@ -14,31 +14,31 @@ CREATE TABLE questions(
 );
 
 CREATE TABLE question_follows(
-    -- id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     author_id INTEGER,
     question_id INTEGER,
     FOREIGN KEY (author_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id),
-    PRIMARY KEY(author_id,question_id)
+    -- PRIMARY KEY(author_id,question_id)
 );
-CREATE TABLE replies(
-    id INTEGER PRIMARY KEY,
-    body TEXT NOT NULL,
-    parent_id INTEGER,
-    user_id INTEGER,
-    question_id INTEGER,
-    FOREIGN KEY (parent_id) REFERENCES replies(id),
-    FOREIGN KEY (user_id) REFERENCES question_follows(author_id)
-    FOREIGN KEY (question_id) REFERENCES questions(id),
-);
-CREATE TABLE question_likes(
-    id INTEGER PRIMARY KEY,
-    user_id INTEGER,
-    question_id INTEGER,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (question_id) REFERENCES questions(id),
-    -- PRIMARY KEY(user_id,question_id)
-);
+-- CREATE TABLE replies(
+--     id INTEGER PRIMARY KEY,
+--     body TEXT NOT NULL,
+--     parent_id INTEGER,
+--     user_id INTEGER,
+--     question_id INTEGER,
+--     FOREIGN KEY (parent_id) REFERENCES replies(id),
+--     FOREIGN KEY (user_id) REFERENCES question_follows(author_id)
+--     FOREIGN KEY (question_id) REFERENCES questions(id),
+-- );
+-- CREATE TABLE question_likes(
+--     id INTEGER PRIMARY KEY,
+--     user_id INTEGER,
+--     question_id INTEGER,
+--     FOREIGN KEY (user_id) REFERENCES users(id),
+--     FOREIGN KEY (question_id) REFERENCES questions(id),
+--     -- PRIMARY KEY(user_id,question_id)
+-- );
 
 
 

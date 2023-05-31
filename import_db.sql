5876daccc06d0a78f8fe0074fefe29f79b72e194
Viktoria Czaran <vczaran@gmail.com>
2023-05-31 17:13:07 -0400

    Working on inserting into tables

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users(
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -12,12 +14,12 @@ CREATE TABLE questions(
 );
 
 CREATE TABLE question_follows(
-    id INTEGER PRIMARY KEY,
+    -- id INTEGER PRIMARY KEY,
     author_id INTEGER,
     question_id INTEGER,
     FOREIGN KEY (author_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id),
-    -- PRIMARY KEY(author_id,question_id)
+    PRIMARY KEY(author_id,question_id)
 );
 CREATE TABLE replies(
     id INTEGER PRIMARY KEY,
@@ -37,6 +39,31 @@ CREATE TABLE question_likes(
     FOREIGN KEY (question_id) REFERENCES questions(id),
     -- PRIMARY KEY(user_id,question_id)
 );
+
+
+
+INSERT INTO 
+    users(fname, lname)
+VALUES
+    ('Manel', 'OS'),
+    ('Viktoria', 'Czaran'),
+    ('Bob', 'Boberson'),
+    ('Chris', 'Brown');
+
+
+INSERT INTO
+    questions(title, body, author_id)
+VALUES
+    ('Favorite Foods', 'What is your favorite food?', (SELECT id FROM users WHERE fname = 'Manel')),
+    ('Favorite Animals', 'What is your favorite animal?', (SELECT id FROM users WHERE fname = 'Viktoria')),
+    ('Existential Crisis', 'Why are we here?', (SELECT id FROM users WHERE fname = 'Bob'));
+
+INSERT INTO
+    question_follows(author_id, question_id)
+VALUES  
+    ((SELECT id FROM users WHERE fname = 'Manel'), (SELECT id FROM questions WHERE title = 'Favorite Foods')),
+    ((SELECT id FROM users WHERE fname = 'Viktoria'), (SELECT id FROM questions WHERE title = 'Favorite Animals')),
+    ((SELECT id FROM users WHERE fname = 'Bob'), (SELECT id FROM questions WHERE title = 'Existential Crisis'));
 
 
 
